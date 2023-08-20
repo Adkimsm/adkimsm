7798322bb7980b553e34d14d3c8dc4088241aabb
adk <75924586+Adkimsm@users.noreply.github.com>
2023-08-20 16:40:27 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 git config --global user.name "Adkimsm"
-git config --global user.email "3020035335@qq.com"
+git config --global user.email "adkinsm9277@gmail.com"
 git add readme.md
 git commit -m 'chore: Update readme.md at '$(date +%Y-%m-%d)
 git pull
